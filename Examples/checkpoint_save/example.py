a1d14750bfc128cf6cade98ee2dbe9519c728f97
seraphli <seraphlivery@gmail.com>
2018-05-28 18:43:04 +0800

    more complex restoration for checkpoint

--- Examples/checkpoint_save/example.py
+++ Examples/checkpoint_save/example.py
@@ -1,5 +1,11 @@
 import os
 import time
+import glob
+
+
+def get_timestamp():
+    import datetime
+    return datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
 class CheckPoint(object):
@@ -10,29 +16,54 @@ class CheckPoint(object):
             import pickle as p_tool
         self._p_tool = p_tool
         self._state = {}
+        self._fn = get_timestamp()
+        self.update('_fn', self._fn)
 
     def update(self, key, value):
         self._state.update({key: value})
 
     def save(self):
-        with open('checkpoint.pkl', 'wb') as f:
+        with open('checkpoint_' + self._fn + '.cp', 'wb') as f:
             self._p_tool.dump(self._state, f)
 
     def restore(self):
-        if os.path.exists('checkpoint.pkl'):
-            cmd = input('Restore checkpoint? [y/n]: ')
-            if cmd in 'y' or cmd in 'Y':
-                with open('checkpoint.pkl', 'rb') as f:
+        cp_files = glob.glob('checkpoint_????????_??????.cp')
+        cp_files = sorted(cp_files, reverse=True)
+        if cp_files:
+            print('Found {} checkpoint files.'.format(len(cp_files)))
+            action = input('Select action ([n]ew/[r]estore/[c]lear): ')
+            if action == 'n' or action == 'new':
+                return False
+            elif action == 'r' or action == 'restore':
+                print('All checkpoint files:')
+                for idx, file in enumerate(cp_files):
+                    print('[{}] {}'.format(idx, file))
+                index = int(input('Input the index of the file[{}-{}]: '.
+                                  format(0, len(cp_files) - 1)))
+                if index > len(cp_files) or index < 0:
+                    print('Index out of range!')
+                    return False
+                with open(cp_files[index], 'rb') as f:
                     self._state = self._p_tool.load(f)
                 self.__dict__.update(self._state)
                 return True
+            elif action == 'c' or action == 'clear':
+                cmd = input('WARN: Clear all checkpoints? [y/N]')
+                if cmd == 'y' or cmd == 'Y':
+                    for file in cp_files:
+                        if os.path.exists(file):
+                            os.remove(file)
+                    print('All checkpoints removed.')
+                    return False
+                else:
+                    return False
             else:
-                self.clear()
+                print('Unrecognized action!')
         return False
 
     def clear(self):
-        if os.path.exists('checkpoint.pkl'):
-            os.remove('checkpoint.pkl')
+        if os.path.exists('checkpoint_' + self._fn + '.cp'):
+            os.remove('checkpoint_' + self._fn + '.cp')
 
 
 class Project(CheckPoint):
